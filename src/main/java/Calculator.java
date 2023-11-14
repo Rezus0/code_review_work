45aa7772ed96cfb692f81e57009b18cd8d7582e5
Andrey Karpushin <karpushin.2003@Inbox.ru>
2023-11-14 05:40:12 +0300

    impl TODO

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,32 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int[] solver(int a, int b, int c) {
+        int discriminant = times(b, b) - times(times(4, a), c);
+        if (discriminant > 0) {
+            int root1 = div((-b + (int) Math.sqrt(discriminant)), (times(2, a)));
+            int root2 = div((-b - (int) Math.sqrt(discriminant)), (times(2, a)));
+            return new int[]{root1, root2};
+        } else if (discriminant == 0) {
+            int root = div(-b, (times(2, a)));
+            return new int[]{root};
+        } else {
+            // Уравнение не имеет целых корней.
+            return new int[0];
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CalculatorTest {
+    private static Calculator calculator;
+
+    @BeforeAll
+    public static void initAll() {
+        calculator = new Calculator();
+    }
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Assertions.assertEquals(4, calculator.add(2, 2));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Assertions.assertEquals(10, calculator.dif(15, 5));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.div(10, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Assertions.assertEquals(12, calculator.times(4, 3));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Assertions.assertArrayEquals(new int[]{2, 0}, calculator.solver(2, -5, 2));
+        Assertions.assertArrayEquals(new int[]{0}, calculator.solver(-4, 0, 0));
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.solver(0, 0, 0));
     }
 }
