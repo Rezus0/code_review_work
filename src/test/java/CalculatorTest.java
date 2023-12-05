46698257eb93866400d8d98dbaa4636a6d830392
Andrey Karpushin <karpushin.2003@Inbox.ru>
2023-12-05 03:27:46 +0300

    refactor Tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
   
